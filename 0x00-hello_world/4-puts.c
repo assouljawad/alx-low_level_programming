0112efed9e4f41af38018f0010832dd0837294dd
assouljawad <assouljawad823@gmail.com>
2023-03-16 18:32:18 -0700

    fixing bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main(){
-char text[] = "Programming is like building a multilingual puzzle";
+int main(void) 
+{
+char text[] = "\"Programming is like building a multilingual puzzle";
 puts(text);
 return 0;
 }

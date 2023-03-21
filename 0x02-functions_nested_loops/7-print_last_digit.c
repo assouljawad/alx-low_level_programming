e1fe5143c5c1137152f63d40737a8fcbb1c6b472
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-21 21:38:06 +0000

    print the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of the number.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int print_last_digit(int n)
+{
+	int rs;
+
+	rs = n % 10;
+	_putchar(rs);
+}

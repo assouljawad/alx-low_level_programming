153cc528fccafe4f7d6f6b8c7f8cf7e9cd2221ea
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-21 21:17:01 +0000

    abs value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - Checks for the abs value of a number.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}

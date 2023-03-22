84b3393179aeec6c2a9ef15b6210305379f74db1
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-22 01:27:21 +0000

    print time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,8 +14,8 @@ void times_table(void)
 		for (b = 1; b <= 9; b++)
 		{
 			op = a * b;
-            _putchar(44);
-            _putchar(32);
+			_putchar(44);
+			_putchar(32);
 			if (op <= 9)
 			{
 				_putchar(32);

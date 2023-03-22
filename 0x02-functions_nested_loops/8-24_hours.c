2f6437b0c3a8482d4f18c5856c69cf521c8374bf
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-22 01:03:41 +0000

    so hard

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - print the last digit of the number.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48 ; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+						{
+							break;
+								putchar(a);
+								putchar(b);
+								putchar(58);
+								putchar(c);
+								putchar(d);
+								putchar('\n');
+						}
+				}
+			}
+		}
+	}
+}

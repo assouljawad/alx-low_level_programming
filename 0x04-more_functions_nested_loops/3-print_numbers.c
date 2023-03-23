b63a6a402c2d35b5462a5b7d08f917617458e289
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-23 16:03:42 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - is functions that checks is the c is digit.
+* Return: if digit return 1 else return 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+}

032399fbb6d986ae7953ef97a3a52ce2395e811f
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-23 18:27:53 +0000

    forgot _ in putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 /**
 * more_numbers - is functions that checks is the c is digit.
 * Return: if digit return 1 else return 0
@@ -8,7 +8,7 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 	for (i = 0; i < 15; i++)
 	{

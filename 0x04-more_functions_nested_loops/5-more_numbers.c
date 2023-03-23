6a9abf0493bb94aa3a81f0c3f4c085d9941617c7
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-23 18:18:38 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* more_numbers - is functions that checks is the c is digit.
+* Return: if digit return 1 else return 0
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+	for (i = 0; i < 15; i++)
+	{
+		if (i >= 10)
+		{
+			putchar((i / 10) + 48);
+		}
+		putchar((i % 10) + 48);
+		}
+		putchar('\n');
+	};
+}

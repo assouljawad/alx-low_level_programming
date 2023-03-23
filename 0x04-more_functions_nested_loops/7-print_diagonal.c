1d078c34b6df5e751caf966e0c6b77d8593762e2
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-23 23:35:52 +0000

    diagonal 3 not fixed wa9ila hhh

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,12 @@ void print_diagonal(int n)
 	{
 	for (i = 0; i < n; i++)
 	{
+		_putchar('\n');
 		for (j = 0; j <= i; j++)
 		{
 			_putchar(' ');
 		}
 		_putchar(92);
-		_putchar('\n');
 	}
 	}
 	else

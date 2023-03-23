2f27771d92afe3a37915cf5f4eed42694560f72b
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-23 15:53:33 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - is functions that checks is the c is digit.
+* @a: holdes the num value
+* @b: holdes the num 2 value
+* Return: if digit return 1 else return 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

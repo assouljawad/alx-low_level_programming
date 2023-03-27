53b89b2b15c2baa0721b1208794b0c73c6413ebe
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-27 18:25:57 +0000

    print-rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_rev  - a poniter to int as argument an change its value.
+* @s: poniter
+* Return: return the lenght of string
+*/
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len - 1; i >= 0; i++)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}

02ad710fc49a6a5aba264e15129aab69af991526
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-27 17:09:10 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _strlen  - a poniter to int as argument an change its value.
+* @s: poniter
+* Return: return the lenght of string
+*/
+void _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}

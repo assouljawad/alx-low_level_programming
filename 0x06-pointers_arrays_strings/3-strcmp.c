ee8faf3001c9041fdf073b317dcebfcbe1d290b3
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-30 21:29:34 +0000

    strcmp last

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,23 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, rs;
 
 	i = 0;
+	rs = 0;
 	while (s1[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			rs = (s1[i] - s2[i]);
+			break;
+		}
+		else
+		{
+			rs = (s1[i] - s2[i]);
+			break;
 		}
 		i++;
 	}
+	return (rs);
 }

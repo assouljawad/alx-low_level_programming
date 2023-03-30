20bca1fa53062015f44d2340f2d4397b75cc12b7
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-03-30 20:59:21 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - comp a string
+ * @s1: destination.
+ * @s2: source.
+ * Return: the pointer to dest.
+ */
+
+char _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+}

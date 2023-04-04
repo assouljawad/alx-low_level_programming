3abc995c21d937cb48bb6c9a80069fe26d196b60
assouljawad <assouljawad823@gmail.com>
2023-04-04 03:42:25 +0200

    strchr fix s[i] != '\0'

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strchr - prints buffer in hexa
+ * @s: the string
+ * @c: the address of memory to print
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}

c14b98b73767212ffc86afbe7b3eda4b8ab93aa6
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-04-11 16:09:42 +0000

    create arry

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,10 +9,11 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p = malloc(sizeof(char) * size);
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
 	}

c14823dd781dbc5c8be6b6f759047128e45b5464
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-04-15 01:48:48 +0000

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* *_realloc - implment of realloc
+* @old_size: size
+* @new_size: size
+* @ptr: ptr
+* Return:newptr
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newptr;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	newptr = malloc(new_size);
+	return (newptr);
+}
